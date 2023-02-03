7f15be2da32e92c25d7dead9d38c4de8683ceb41
ChiaYang <aa235553756@gmail.com>
2023-02-03 15:09:09 +0800

    fix checkAuthFn

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { checkAuthApi } from "../helpers/apiTest";
+import { useAuth } from "../helpers/Context";
 import { Navbar } from "/src/containers/Navbar";
 import { TodoList } from "/src/containers/TodoList";
 import '/src/scss/todo.scss'
 
 export function TodoPage() {
+  const { setToken } = useAuth();
   //假設local有token,但已經過期,此時進入首頁,有token,ProtectRoute會失效，就需要以下程式碼
   //ProtectRoutea沒有通過這頁(元件)也進不來,(連api都不打)
   const navigate = useNavigate()
